69730e708964e6f3bcb377153cda295048d196d5
Botond Fekete <fekbot@gmail.com>
2023-10-03 00:36:38 +0100

    Change invite function to use firestore

--- functions/src/on-event-create.ts
+++ functions/src/on-event-create.ts
@@ -1,78 +1,81 @@
-import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
-import * as logger from "firebase-functions/logger";
-// @ts-ignore
-import Mailgun from 'mailgun-js';
-import * as formData from 'form-data';
+// import * as functions from "firebase-functions";
+// import * as admin from "firebase-admin";
+// import * as logger from "firebase-functions/logger";
+// // @ts-ignore
+// import Mailgun from "mailgun-js";
+// import * as formData from "form-data";
 
-admin.initializeApp();
-const db = admin.database();
+// admin.initializeApp();
+// const db = admin.database();
 
-const mailgun = new Mailgun(formData);
+// const mailgun = new Mailgun(formData);
 
-const MAILGUN_DOMAIN = functions.config().mailgun.domain;
-const MAILGUN_API_KEY = functions.config().mailgun.key;
+// const MAILGUN_DOMAIN = functions.config().mailgun.domain;
+// const MAILGUN_API_KEY = functions.config().mailgun.key;
 
-// Initialize Mailgun
-const mg = mailgun.client({
-  user: 'api',
-  key: MAILGUN_API_KEY,
-});
+// // Initialize Mailgun
+// const mg = mailgun.client({
+//   user: "api",
+//   key: MAILGUN_API_KEY,
+// });
 
-export const onEventCreation = functions.database.ref('events/{id}')
-  .onCreate((snapshot) => {
-    const data = snapshot.val();
+// export const onEventCreation = functions.database.ref("events/{id}")
+//   .onCreate((snapshot) => {
+//     const data = snapshot.val();
 
-    // TODO: send email to all members (invites/accepted=true)
-    // db.ref('invites').on('value', snapshots => {
-    //   const invites = snapshots.val().map(child => child.val())
-    // })
+//     // TODO: send email to all members (invites/accepted=true)
+//     // db.ref('invites').on('value', snapshots => {
+//     //   const invites = snapshots.val().map(child => child.val())
+//     // })
 
-    const members: string[] = []
+//     const members: string[] = [];
 
-    // Email details
-    const createMailOptions = {
-      from: 'Pencilheads <info@pencilheads.net>',
-      to: data.email,
-      subject: `${data.createdBy.name} has created a new event!`,
-      template: 'Event',
-      'h:X-Mailgun-Variables': JSON.stringify({
-        person: data.createdBy.name,
-        inviteToken: snapshot.key,
-      }),
-    };
+//     // Email details
+//     const createMailOptions = {
+//       "from": "Pencilheads <info@pencilheads.net>",
+//       "to": data.email,
+//       "subject": `${data.createdBy.name} has created a new event!`,
+//       "template": "Event",
+//       "h:X-Mailgun-Variables": JSON.stringify({
+//         person: data.createdBy.name,
+//         inviteToken: snapshot.key,
+//       }),
+//     };
 
-    // Email details
-    const inviteMailOptions = {
-      from: 'Pencilheads <info@pencilheads.net>',
-      to: members.join(','),
-      subject: `${data.createdBy.name} has created a new event!`,
-      template: 'Event',
-      'h:X-Mailgun-Variables': JSON.stringify({
-        person: data.createdBy.name,
-        inviteToken: snapshot.key,
-      }),
-    };
+//     // Email details
+//     const inviteMailOptions = {
+//       "from": "Pencilheads <info@pencilheads.net>",
+//       "to": members.join(","),
+//       "subject": `${data.createdBy.name} has created a new event!`,
+//       "template": "Event",
+//       "h:X-Mailgun-Variables": JSON.stringify({
+//         person: data.createdBy.name,
+//         inviteToken: snapshot.key,
+//       }),
+//     };
 
-    // Send the email using Mailgun
-    return Promise.all([
-      mg.messages.create(MAILGUN_DOMAIN, createMailOptions)
-        .then(() => {
-          logger.log('Event creation email sent to:', data.email);
-        })
-        .catch((error: any) => {
-          logger.error('There was an error while sending the email:', error);
-        }),
-      async () => {
-        if (members.length > 0) {
-          return mg.messages.create(MAILGUN_DOMAIN, inviteMailOptions)
-            .then(() => {
-              logger.log('Event invite email sent to:', members.join(','));
-            })
-            .catch((error: any) => {
-              logger.error('There was an error while sending the email:', error);
-            })
-        }
-      },
-    ]);
-  });
+//     // Send the email using Mailgun
+//     return Promise.all([
+//       mg.messages.create(MAILGUN_DOMAIN, createMailOptions)
+//         .then(() => {
+//           logger.log("Event creation email sent to:", data.email);
+//         })
+//         .catch((error: any) => {
+//           logger.error("There was an error while sending the email:", error);
+//         }),
+//       async () => {
+//         if (members.length > 0) {
+//           return mg.messages.create(MAILGUN_DOMAIN, inviteMailOptions)
+//             .then(() => {
+//               logger.log("Event invite email sent to:", members.join(","));
+//             })
+//             .catch((error: any) => {
+//               logger.error(
+//                "There was an error while sending the email:",
+//                error
+//              );
+//             });
+//         }
+//       },
+//     ]);
+//   });
--- functions/src/on-invite-create.ts
+++ functions/src/on-invite-create.ts
@@ -1,11 +1,11 @@
-import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 import * as logger from "firebase-functions/logger";
-// @ts-ignore
-import Mailgun from 'mailgun-js';
-import * as formData from 'form-data';
+import Mailgun from "mailgun.js";
+import formData from "form-data";
 
 admin.initializeApp();
+const db = admin.firestore();
 
 const mailgun = new Mailgun(formData);
 
@@ -14,32 +14,40 @@ const MAILGUN_API_KEY = functions.config().mailgun.key;
 
 // Initialize Mailgun
 const mg = mailgun.client({
-  user: 'api',
+  username: "api",
   key: MAILGUN_API_KEY,
 });
 
-export const onEventCreate = functions.database.ref('events/{id}')
-  .onCreate((snapshot) => {
-    const data = snapshot.val();
+export const onInviteCreate = functions.firestore.document("invites/{id}")
+  .onCreate(async (snapshot, context) => {
+    const data = snapshot.data();
+
+    if (!data) {
+      logger.error("Data not found for document:", context.params.id);
+      return;
+    }
+
+    const space = await db.collection("spaces").doc(data.spaceId).get();
+    const spaceName = space.data()?.name;
 
     // Email details
     const mailOptions = {
-      from: 'Pencilheads <info@pencilheads.net>',
-      to: data.email,
-      subject: 'You have been invited!',
-      template: 'Invitation',
-      'h:X-Mailgun-Variables': JSON.stringify({
+      "from": "Pencilheads <info@pencilheads.net>",
+      "to": data.email,
+      "subject": `You have been invited to join ${spaceName}!`,
+      "template": "Invitation",
+      "h:X-Mailgun-Variables": JSON.stringify({
         person: data.createdBy.name,
-        inviteToken: snapshot.key,
+        space: spaceName,
+        inviteToken: context.params.id,
       }),
     };
 
     // Send the email using Mailgun
-    return mg.messages.create(MAILGUN_DOMAIN, mailOptions)
-      .then(() => {
-        logger.log('Email sent to:', data.email);
-      })
-      .catch((error: any) => {
-        logger.error('There was an error while sending the email:', error);
-      });
+    try {
+      await mg.messages.create(MAILGUN_DOMAIN, mailOptions);
+      logger.log("Email sent to:", data.email);
+    } catch (error) {
+      logger.error("There was an error while sending the email:", error);
+    }
   });
