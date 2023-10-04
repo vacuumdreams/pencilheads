81c49d02aaadf88a40d511d965d59669600d10a8
Botond Fekete <fekbot@gmail.com>
2023-10-04 16:35:15 +0100

    Add copy button to telegram settings

--- src/pages/events/dashboard/group/settings.tsx
+++ src/pages/events/dashboard/group/settings.tsx
@@ -1,5 +1,8 @@
+import React from 'react';
 import { useForm } from 'react-hook-form'
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { useMutate } from '@/hooks/use-mutate'
+import { Icons } from '@/components/icons'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Space } from '@/types'
@@ -16,13 +19,22 @@ type GroupSettingsProps = {
 }
 
 export const GroupSettings = ({ isAdmin, space, spaceId }: GroupSettingsProps) => {
+  const [justCopied, setJustCopied] = React.useState<string | null>(null)
   const { update, loading } = useMutate<Partial<Space>>()
-  const { register, handleSubmit, formState } = useForm<FormData>({
+  const { register, handleSubmit, formState, getValues } = useForm<FormData>({
     defaultValues: {
       telegramGroupId: space.telegramGroupId,
       telegramInviteLink: space.telegramInviteLink
     }
   })
+
+  React.useEffect(() => {
+    if (justCopied) {
+      setTimeout(() => {
+        setJustCopied(null)
+      }, 3000)
+    }
+  }, [justCopied])
 
   const onSubmit = handleSubmit((data) => {
     update(`spaces/${spaceId}`, data)
@@ -30,19 +42,46 @@ export const GroupSettings = ({ isAdmin, space, spaceId }: GroupSettingsProps) =
 
   return (
     <form onSubmit={onSubmit}>
-      <div className="mb-4">
-        <Input
-          disabled={!isAdmin}
-          {...register('telegramGroupId')}
-        />
+      <div className="w-full flex gap-2 mb-4 items-end">
+        <div className="w-full">
+          <label htmlFor='telegramGroupId'>
+            Telegram group ID
+          </label>
+          <Input
+            id="telegramGroupId"
+            disabled={!isAdmin}
+            {...register('telegramGroupId')}
+          />
+        </div>
+        <CopyToClipboard text={getValues('telegramGroupId')} onCopy={() => setJustCopied('telegramGroupId')}>
+          <Button variant="outline">
+            {justCopied === 'telegramGroupId' ? <Icons.check width={12} /> : <Icons.copy width={12} />}
+          </Button>
+        </CopyToClipboard>
       </div>
-      <div className="mb-4">
-        <Input
-          disabled={!isAdmin}
-          {...register('telegramInviteLink')}
-        />
+      <div className="w-full flex gap-2 mb-4 items-end">
+        <div className="w-full">
+          <label htmlFor='telegramInviteLink'>
+            Telegram invite link
+          </label>
+          <Input
+            id="telegramInviteLink"
+            disabled={!isAdmin}
+            {...register('telegramInviteLink')}
+          />
+        </div>
+        <CopyToClipboard text={getValues('telegramInviteLink')} onCopy={() => setJustCopied('telegramInviteLink')}>
+          <Button variant="outline">
+            {justCopied === 'telegramInviteLink' ? <Icons.check width={12} /> : <Icons.copy width={12} />}
+          </Button>
+        </CopyToClipboard>
       </div>
-      <Button disabled={!formState.isValid || !isAdmin || loading}>Save</Button>
+      <Button
+        type="submit"
+        disabled={!formState.isValid || !formState.isDirty || !isAdmin || loading}
+      >
+        Save
+      </Button>
     </form>
   )
 }
